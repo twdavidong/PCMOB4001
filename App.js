f7fa8ca741bfb605be94e3376558ee1b67dd45e3
twdavidong <64457327+twdavidong@users.noreply.github.com>
2021-05-20 21:00:42 +0800

    Commit001

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [loading, setLoading] = useState(true);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bus arrival time:</Text>
+      <Text style={styles.arrivalTime}>
+        {loading ? "Loading...": "Loaded"}
+      </Text>
+    <TouchableOpacity style={styles.button}>
+      <Text style={styles.buttonText}>Refresh!</Text>
+    </TouchableOpacity>
     </View>
   );
 }
